f0aa7161b8500be9c074a8bfe40ab70b62f1e916
Rezeh Edwine <soshamedia.marketing@googlemail.com>
2025-12-03 12:05:22 +0100

    update login page

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,9 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250828',
-    'hash': 'b92f79e2'
-  }
+    'version': '251203',
+    'hash': '5ad3085c'
+  },
+  'allow_switching_backend_instance': true
 };
 /* tslint:enable */
